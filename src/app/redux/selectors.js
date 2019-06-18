aee3406b04e6ab3495894376911241ec9e97b761
Ivan Banov <ivanbanov@gmail.com>
2019-06-18 02:56:49 +0200

    Fix search

--- src/app/redux/selectors.js
+++ src/app/redux/selectors.js
@@ -1,12 +1,17 @@
 import removeAccents from 'remove-accents';
 
-export const tasksForSearch = (state) =>
-  state.get('tasks').filter((task) => {
+export const tasksForSearch = (state) => {
+  if (!state.get('search')) {
+    return state.get('tasks');
+  }
+
+  return state.get('tasks').filter((task) => {
     const parsedText = JSON.stringify(state.get('search')).slice(1, -1);
     const withoutAccents = removeAccents(parsedText);
     const regExp = new RegExp(withoutAccents, 'i');
     return regExp.test(task.get('text'));
   });
+};
 
 export const tasksByStatus = (state) =>
   tasksForSearch(state)
